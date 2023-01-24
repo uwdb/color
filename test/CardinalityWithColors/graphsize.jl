9a9290e66d529cd10d8efbe86c9143644e18657a
diandremiguels <diandresabale@gmail.com>
2023-01-23 16:21:21 -0800

    finished 1-edge graph tests

--- test/CardinalityWithColors/graphsize.jl
+++ test/CardinalityWithColors/graphsize.jl
@@ -6,10 +6,33 @@ using Graphs
 
 @testset "symmetrical graphs" begin
     @testset "1-edge graph" begin
-        
+        numVertices = 2
+        g = path_graph(numVertices)
+        summary = generate_color_summary(g, 16)
+        query_graph = DiGraph(2)
+        add_edge!(query_graph, (1, 2))
+        exact_size = only(get_exact_size(query_graph, g; verbose=false))
+        predicted_size = 2
+        @test predicted_size == exact_size
+    end
+
+    @testset "query larger than 1-edge graph" begin
+        numVertices = 2
+        g = path_graph(numVertices)
+        summary = generate_color_summary(g, 16)
+        query_graph = DiGraph(3)
+        add_edge!(query_graph, (1, 2))
+        add_edge!(query_graph, (2,3))
+        exact_size = only(get_exact_size(query_graph, g; verbose=false))
+        predicted_size = 2
+        @test predicted_size == exact_size
     end
     
     @testset "cycle graph" begin
+        
+    end
+
+    @testset "query larger than cycle graph" begin
         
     end
 
