57f31f5f934050e0b0e2e4475efd9041b06679ba
kylebd99 <kylebd99@gmail.com>
2023-02-15 10:28:33 -0800

    Add add_labeled_edge! and add_labeled_node! funcs

--- Source/UnlabeledCardinalityEstimator.jl
+++ Source/UnlabeledCardinalityEstimator.jl
@@ -13,8 +13,30 @@ struct PropertyGraph
     graph::DiGraph
     edge_labels::Dict{Int, Dict{Int, Array{Int}}} # edge_labels[n1][n2] = { labels }
     vertex_labels::Dict{Int, Array{Int}} # vertex_labels[n] = { labels }
+
+    PropertyGraph(num_vertices::Int) = new(DiGraph(num_vertices), Dict(), Dict())
 end
 
+
+function add_labeled_node!(g::PropertyGraph, node::Int, node_labels::Array{Int})
+    g.vertex_labels[node] = node_labels
+end
+
+function add_labeled_edge!(g::PropertyGraph, edge::Tuple{Int, Int}, edge_label::Int)
+    add_edge!(g.graph, edge)
+    parent_node = edge[1]
+    child_node = edge[2]
+    if !haskey(g.edge_labels, parent_node)
+        g.edge_labels[parent_node] = Dict()
+    end
+    if !haskey(g.edge_labels[parent_node], child_node)
+        g.edge_labels[parent_node][child_node] = []
+    end
+    if edge_label in g.edge_labels[parent_node][child_node]
+        return
+    end
+    push!(g.edge_labels[parent_node][child_node], edge_label)
+end 
 # Everywhere that we reference a graph (query or data) we should replace it with our new struct.
 
 # Extend the colorsummary to account for edge labels and vertex labels as additional layers of dict nesting
