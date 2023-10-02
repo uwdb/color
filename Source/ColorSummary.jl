fd00b0685b7200e7758234ad6a6e16b07b892844
Diandre Sabale <dmbs@dragon.cs.washington.edu>
2023-10-02 12:23:35 -0700

    removed extra comments

--- Source/ColorSummary.jl
+++ Source/ColorSummary.jl
@@ -744,31 +744,6 @@ end
 
 # gets the directed path from the start to finish node
 function get_matching_graph(start::Int, finish::Int, query::QueryGraph)
-    # use yen's algorithm to find the shortest distances between source and target vertex
-    # ys = yen_k_shortest_paths(query.graph, start, finish, K=3)
-    # # find the first path that isn't a direct connections
-    # index = 1
-    # for i in eachindex(ys.dists)
-    #     if ys.dists[i] > 1
-    #         index = i
-    #         break
-    #     end
-    # end
-    # # this new path is a series of vertices
-    # shortest_path = ys.paths[index]
-    # new_graph = DiGraph(length(shortest_path))
-    # current_start = 0
-    # for path_index in 1:(length(shortest_path)-1)
-    #     current_start += 1
-    #     if shortest_path[path_index] in outneighbors(query.graph, shortest_path[path_index + 1])
-    #         # this is a backwards edge
-    #         add_edge!(new_graph, current_start + 1, current_start)
-    #     else
-    #         # this is a forwards edge
-    #         add_edge!(new_graph, current_start, current_start + 1)
-    #     end
-    # end
-
     # convert the graph to be undirected
     graph_copy = Graph(copy(query.graph))
     rem_edge!(graph_copy, start, finish)
