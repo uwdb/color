bb62125f645e175a700f42b1caaac5cbb8b36a78
kylebd99 <kdeeds@cs.washington.edu>
2023-10-18 16:20:12 -0700

    add memory exps

--- Experiments/Scripts/cycle_prob_exps.jl
+++ Experiments/Scripts/cycle_prob_exps.jl
@@ -7,15 +7,19 @@ datasets = [aids, yeast, hprd, dblp, youtube, wordnet]
 experiment_params = Vector{ExperimentParams}()
 build_params = Vector{ExperimentParams}()
 for dataset in datasets
-    push!(build_params, ExperimentParams(dataset=dataset))
+    push!(build_params, ExperimentParams(dataset=dataset,
+                        num_colors=16,
+                        label_refining_rounds=2))
     for only_shortest_path_cycle in [false, true]
         push!(experiment_params, ExperimentParams(dataset=dataset,
-                                                  only_shortest_path_cycle=only_shortest_path_cycle))
+                                                    num_colors=16,
+                                                    label_refining_rounds=2,
+                                                    only_shortest_path_cycle=only_shortest_path_cycle))
     end
 end
 
-#build_experiments(build_params)
+build_experiments(build_params)
 
-#run_estimation_experiments(experiment_params)
+run_estimation_experiments(experiment_params)
 
-graph_grouped_box_plot(experiment_params; grouping=cycle_stats, filename="cycle_stats_exps")
+graph_grouped_box_plot(experiment_params; grouping=cycle_stats, filename="cycle_stats_exps_w_refining")
