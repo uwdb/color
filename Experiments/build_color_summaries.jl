3516583c8b470e170c126e6a12bb4039c1fb67d9
kylebd99 <kdeeds@cs.washington.edu>
2023-12-15 13:16:04 -0800

    multi-thread color summary build

--- Experiments/build_color_summaries.jl
+++ Experiments/build_color_summaries.jl
@@ -1,5 +1,5 @@
 function build_experiments(experiment_params_list::Vector{ExperimentParams})
-    for experiment_params in experiment_params_list
+    Threads.@threads for experiment_params in experiment_params_list
         build_times = [("Dataset", "Partitioner", "NumColors",  "BuildPhase", "BuildTime", "MemoryFootprint")]
         dataset = experiment_params.dataset
         summary_params = experiment_params.summary_params
