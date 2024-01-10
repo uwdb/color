9d686ad545e5223b0dde9da8f7efe2979c1714cf
Diandre Sabale <dmbs@dragon.cs.washington.edu>
2024-01-10 10:57:20 -0800

    experiment changes

---
+++ Experiments/Scripts/partial-sums-exps.jl
@@ -0,0 +1,84 @@
+using Plots.PlotMeasures
+include("../Experiments.jl")
+
+current_dataset = aids
+max_paths = 9999999
+
+
+# partial summation experiments
+# for each width, graph the inference time for partial aggregation (no sampling), no partial aggregation (no sampling), partial aggregation with inference sampling
+
+experiment_params_list::Vector{ExperimentParams} = [ExperimentParams(dataset=current_dataset, use_partial_sums=true, inference_max_paths=max_paths),
+                                                    ExperimentParams(dataset=current_dataset, use_partial_sums=false, inference_max_paths=max_paths),
+                                                    ExperimentParams(dataset=current_dataset, use_partial_sums=true)]
+println("started building")
+build_experiments(experiment_params_list)
+println("started estimating")
+run_estimation_experiments(experiment_params_list, timeout=TIMEOUT_SEC)
+println("started graphing")
+
+x_values = []
+y_values = []
+groups = []
+for experiment_params in experiment_params_list
+    # load the results
+    results_filename = params_to_results_filename(experiment_params)
+    results_path = "Experiments/Results/Estimation_" * results_filename
+    results_df = CSV.read(results_path, DataFrame; normalizenames=true)
+    # keep track of the data points
+    for i in 1:nrow(results_df)
+        failure = results_df[i, :Failure]
+        if failure
+            continue
+        end
+        current_x = results_df[i, :PathWidth]
+        current_group = "No Partial Agg"
+        if experiment_params.use_partial_sums
+            current_group = "Partial Agg"
+            if experiment_params.inference_max_paths != max_paths
+                current_group *= " w/ Sampling"
+            end
+        end
+        current_y = results_df[i, :EstimationTime]
+        push!(x_values, current_x)
+        push!(y_values, current_y)
+        push!(groups, current_group)
+    end
+end
+x_order = sort(unique(x_values))
+x_ticks = ([x for x in 1:length(x_order)], x_order)
+x_order_hash = [hash(x) for x in x_order]
+x_values = [only(indexin(hash(x), x_order_hash)) for x in x_values]
+results_filename = params_to_results_filename(experiment_params_list[1])
+println("starting graphs")
+
+# This seems to be necessary for using Plots.jl outside of the ipynb framework.
+# See this: https://discourse.julialang.org/t/deactivate-plot-display-to-avoid-need-for-x-server/19359/15
+ENV["GKSwstype"]="100"
+ylims=[0, 10^2.5]
+y_ticks=[10^-10, 10^-5, 10^-2, 1, 10^2, 10^5, 10^10]
+dimensions = (1000, 800)
+gbplot = groupedboxplot(x_values,
+                        [log10(y) for y in y_values],
+                        group = groups,
+                        x_ticks = x_ticks,
+                        xlims = [0, length(x_order) + .5],
+                        ylims =  (log10(ylims[1]),log10(ylims[2])),
+                        y_ticks = [log10(y) for y in y_ticks],
+                        legend = :outerright,
+                        size = dimensions,
+                        bottom_margin = 20px,
+                        top_margin = 20px,
+                        left_margin = 10mm,
+                        legend_column = 1,
+                        titlefont = (12, :black),
+                        legendfont = (11, :black),
+                        tickfont = (12, :black),
+                        guidefont = (15, :black),
+                        whisker_range=2)
+xlabel!(gbplot, "Query Path Width")
+ylabel!(gbplot, "Inference Latency 10^ (s)")
+plotname = "partial-agg-exp.png"
+savefig(gbplot, "Experiments/Results/Figures/" * plotname)
+
+# graph_grouped_box_plot(experiment_params_list, x_type=query_type, y_type=estimate_error, grouping=summary_paths, filename="summarysamplesquerytypesyeast")
